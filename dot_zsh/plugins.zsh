ebd587f27c64386ad972c286377d132453a26ffc
poiuy17 <18458395+poiuy17@users.noreply.github.com>
2024-10-16 16:50:57 -0500

    update chezmoi on windows

--- dot_zsh/plugins.zsh
+++
@@ -1,8 +0,0 @@
-# Syntax highlighting
-[[ -f ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]] && source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-# Auto suggestion
-[[ -f ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ]] && source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-# zsh-completions
-[[ -d ~/.zsh/plugins/zsh-completions/src ]] && fpath=(~/.zsh/plugins/zsh-completions/src $fpath)
