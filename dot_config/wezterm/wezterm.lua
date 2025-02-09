a8118b777664469ba33d3b18267530357fb1aa00
poiuy17 <18458395+poiuy17@users.noreply.github.com>
2025-02-09 16:31:15 -0600

    update wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,74 +1,62 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
--- local config = wezterm.config_builder()
 local config = {}
-
--- Mux is the mutliplexes for windows etc inside of the terminal
--- Action is to perform actions on the terminal
 local mux = wezterm.mux
 local act = wezterm.action
 
--- This is for newer wezterm vertions to use the config builder
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  config.default_prog = { "pwsh.exe", "-NoLogo" }
+if string.find(wezterm.target_triple, "windows") then
+  config.default_domain = 'WSL:Ubuntu'
 end
 
-config.color_scheme = 'GruvboxDark'
-config.font = wezterm.font 'Hack Nerd Font'
+-- config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+
+config.color_scheme = 'Nord (Gogh)'
+config.font = wezterm.font 'FiraCode Nerd Font'
 config.font_size = 14
+config.line_height = 1.0
 
-config.window_decorations = "RESIZE"
-
-config.use_fancy_tab_bar = false
 config.window_background_opacity = 0.9
 
-config.inactive_pane_hsb = {
-  saturation = 0.3,
-  brightness = 0.3,
-}
+config.hide_tab_bar_if_only_one_tab = true
 
--- Key Bingind
 config.disable_default_key_bindings = true
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
 config.keys = {
-  -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-  { key = 'a',          mods = 'LEADER|CTRL', action = wezterm.action { SendString = "\x01" } },
-  { key = "v",          mods = 'SHIFT|CTRL',  action = act.PasteFrom 'Clipboard' },
-  { key = "c",          mods = "SHIFT|CTRL",  action = act.CopyTo("Clipboard") },
-
-  { key = "h",          mods = "ALT",         action = wezterm.action.ActivatePaneDirection("Left"), },
-  { key = "j",          mods = "ALT",         action = wezterm.action.ActivatePaneDirection("Down"), },
-  { key = "k",          mods = "ALT",         action = wezterm.action.ActivatePaneDirection("Up"), },
-  { key = "l",          mods = "ALT",         action = wezterm.action.ActivatePaneDirection("Right"), },
-
-  { key = '-',          mods = "LEADER",      action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" }, }), },
-  { key = '\\',          mods = "LEADER",      action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" }, }), },
-
-  { key = "x",          mods = "LEADER",      action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-  { key = "c",          mods = "LEADER",      action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
-  { key = "z",          mods = "LEADER",      action = "TogglePaneZoomState" },
-
-  { key = "LeftArrow",  mods = "SHIFT|CTRL",  action = act.AdjustPaneSize({ "Left", 1 }) },
-  { key = "UpArrow",    mods = "SHIFT|CTRL",  action = act.AdjustPaneSize({ "Up", 1 }) },
-  { key = "DownArrow",  mods = "SHIFT|CTRL",  action = act.AdjustPaneSize({ "Down", 1 }) },
-  { key = "RightArrow", mods = "SHIFT|CTRL",  action = act.AdjustPaneSize({ "Right", 1 }) },
-
+  { key = "v", mods = 'SHIFT|CTRL', action = act.PasteFrom 'Clipboard' },
 }
 
 config.mouse_bindings = {
-  -- Ctrl-click will open the link under the mouse cursor
   {
-    event = { Up = { streak = 1, button = "Left" } },
-    mods = "CTRL",
-    action = wezterm.action.OpenLinkAtMouseCursor,
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'NONE',
+    action = act.CompleteSelection 'ClipboardAndPrimarySelection',
+  },
+  {
+    event = { Down = { streak = 1, button = 'Middle' } },
+    mods = 'NONE',
+    action = act.Nop,
+  },
+  {
+    event = { Up = { streak = 1, button = 'Middle' } },
+    mods = 'NONE',
+    action = act.OpenLinkAtMouseCursor,
+  },
+  {
+    event = { Up = { streak = 1, button = 'Right' } },
+    mods = 'NONE',
+    action = act.PasteFrom 'Clipboard',
   },
 }
 
--- and finally, return the configuration to wezterm
+config.selection_word_boundary = " \t\n{}[]()<>\"'`"
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
 return config
